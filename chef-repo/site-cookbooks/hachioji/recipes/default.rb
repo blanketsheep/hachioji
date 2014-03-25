8a32c26789833bf6dc69f0dea97b409de7936315
Shuma Yoshioka <s64.stdio@gmail.com>
2014-03-25 21:37:14 +0900

    Create cookbook

---
+++ chef-repo/site-cookbooks/hachioji/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: hachioji
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+yum_package 'ruby'
+yum_package 'ruby-devel'
+gem_package 'bundler'
