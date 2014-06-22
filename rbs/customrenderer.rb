d3a0dd8f2ecba9c54d007eb8c1a618108b35d27c
Shuma Yoshioka <s64.stdio@gmail.com>
2014-06-22 21:08:59 +0900

    Update customrenderer.rb

--- rbs/customrenderer.rb
+++ rbs/customrenderer.rb
@@ -3,7 +3,7 @@ require 'middleman-core/renderers/redcarpet'
 class CustomRedcarpetRenderer < Middleman::Renderers::MiddlemanRedcarpetHTML
   
   def postprocess(full_document)
-    full_document.gsub(/\[flickr:(.+?)\/(.+)\]/, '<iframe class="flickrembed" src="https://www.flickr.com/photos/\1/\2/player/" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>')
+    full_document.gsub(/\[flickr:(.+?)\]/, '<iframe class="flickrembed" src="https://www.flickr.com/photos/\1/player/" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>')
   end
   
 end
